0ce22f97fa1a7fd14a9b644ea93f8b7ce5222f7e
Costin Ionescu <costin.ionescu@gmail.com>
2016-04-29 07:06:37 -0700

    added doxygen documentation

--- hbs.h
+++ hbs.h
@@ -1,6 +1,36 @@
 #ifndef _HBS_H
 #define _HBS_H
 
+/** @mainpage Host Basic Services
+ *
+ *  This is a library implementing interfaces defined in Zalmoxis library.
+ *  Here is all OS-specific functionality implemented, allowing a wide set of portable
+ *  tools to depend only on zlx and hbs.
+ *
+ *  @section License
+ *
+ *  Copyright (c) 2016, Costin Ionescu <costin.ionescu@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *  
+ *  Note: ISC license (functionally equivalent to simplified BSD and MIT/Expat)
+ */
+
+/** @defgroup hbs Host Basic Services
+ *  @{
+ */
+
 #include <zlx.h>
 
 #ifdef _WIN32
@@ -17,6 +47,14 @@
 #define HBS_API ZLX_LIB_IMPORT
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* hbs_main_func_t **********************************************************/
+/**
+ *  Function pointer type for the @a main function of a program.
+ */
 typedef uint8_t (ZLX_CALL * hbs_main_func_t) 
     (unsigned int argc, uint8_t const * const * argv);
 
@@ -29,34 +67,94 @@ HBS_API int hbs_win_main (int argc, wchar_t const * const * argv, hbs_main_func_
 #else
 HBS_API int hbs_posix_main (int argc, char const * const * argv, 
                             hbs_main_func_t main_func);
+
+/*  HBS_MAIN  */
+/**
+ *  Macro that implements the target environment's execution stub that calls the user provided
+ *  main function.
+ */
 #define HBS_MAIN(_func) \
     uint8_t ZLX_CALL _func (unsigned int argc, uint8_t const * const * argv); \
     int main (int argc, char const * const * argv) { \
         return hbs_posix_main(argc, argv, _func); }
 #endif
 
+/*  hbs_status_t  */
+/**
+ *  Status code for hbs library-specific functions.
+ */
 typedef enum hbs_status_enum hbs_status_t;
 
 enum hbs_status_enum
 {
+    /** Success */
     HBS_OK = 0,
+
+    /** Generic failure status */
     HBS_FAILED,
+
+    /** Not enough memory */
     HBS_NO_MEM,
+
+    /** Not enough resources */
     HBS_NO_RES,
+
+    /** Deadlock detected */
     HBS_DEADLOCK,
+
+    /** Thread already joining another thread */
     HBS_ALREADY_JOINING,
+
+    /** No such thread */
     HBS_NO_THREAD,
+
+    /** Invalid path */
     HBS_BAD_PATH,
+
+    /** Invalid file descriptor */
     HBS_BAD_FILE_DESC,
 
+    /** Functionality not implemented yet */
     HBS_TODO = 0x7E,
+
+    /** Bug detected. On debug and checked builds an assert usually trips 
+     *  instead of receiving this status code, but in some cases this 
+     *  can be obtained even in release. */
     HBS_BUG = 0x7F
 };
 
+/*  hbs_lib_name  */
+/**
+ *  Static string describing the flavour of hbs library.
+ */
 extern HBS_API char const * const hbs_lib_name;
+
+/*  hbs_in  */
+/**
+ *  File object for application's standard input.
+ *  This is valid after hbs_init().
+ */
 extern HBS_API zlx_file_t * hbs_in;
+
+/*  hbs_out  */
+/**
+ *  File object for application's standard output.
+ *  This is valid after hbs_init().
+ */
 extern HBS_API zlx_file_t * hbs_out;
+
+/*  hbs_err  */
+/**
+ *  File object for application's standard error.
+ *  This is valid after hbs_init().
+ */
 extern HBS_API zlx_file_t * hbs_err;
+
+
+/*  hbs_ma  */
+/**
+ *  The default memory allocator.
+ */
 extern HBS_API zlx_ma_t * hbs_ma;
 
 /* hbs_init *****************************************************************/
@@ -93,10 +191,28 @@ HBS_API void ZLX_CALL hbs_finish ();
 /* multi-threading                                                          */
 /****************************************************************************/
 
+/*  hbs_thread_func_t  */
+/**
+ *  Thread function pointer.
+ */
 typedef uint32_t (* ZLX_CALL hbs_thread_func_t) (void * arg);
 
+/*  hbs_thread_t  */
+/**
+ *  Integral type used as thread identifier.
+ */
 typedef uintptr_t hbs_thread_t;
+
+/*  hbs_mutex_t  */
+/**
+ *  Opaque type for mutex.
+ */
 typedef struct hbs_mutex_s hbs_mutex_t;
+
+/*  hbs_cond_t  */
+/**
+ *  Opaque type for Condition Variable.
+ */
 typedef struct hbs_cond_s hbs_cond_t;
 
 typedef struct hbs_thread_start_s hbs_thread_start_t;
@@ -129,7 +245,7 @@ HBS_API hbs_status_t ZLX_CALL hbs_thread_join
 
 /* hbs_mutex_size ***********************************************************/
 /**
- *  Returns the size of a mutex.
+ *  Gives the size of a mutex.
  */
 extern HBS_API size_t hbs_mutex_size;
 
@@ -242,6 +358,9 @@ HBS_API void ZLX_CALL hbs_cond_wait
 );
 
 /* hbs_cond_create **********************************************************/
+/**
+ *  Allocates and initializes an condition variable.
+ */
 ZLX_INLINE hbs_status_t hbs_cond_create (hbs_cond_t * * cond_pp)
 {
     hbs_status_t hs;
@@ -265,7 +384,7 @@ ZLX_INLINE void hbs_cond_destroy (hbs_cond_t * cond_p)
 
 /* hbs_file_from_posix_fd ***************************************************/
 /**
- *  Generates a file object from a file descriptor.
+ *  Generates a file object from a POSIX file descriptor.
  *  @param fp [out]
  *      pointer to be filled with the pointer to the newly created file object
  *  @param fd [in]
@@ -284,13 +403,16 @@ HBS_API hbs_status_t ZLX_CALL hbs_file_from_posix_fd
 );
 
 /* hbs_file_from_windows_handle *********************************************/
+/**
+ *  Generates a file object from a Windows file handle.
+ *  @warning this function is available only on Windows.
+ */
 HBS_API hbs_status_t ZLX_CALL hbs_file_from_windows_handle
 (
     zlx_file_t * * fp,
     void * file_hnd,
     uint32_t flags
 );
-
 
 /* hbs_file_open_ro *********************************************************/
 /**
@@ -334,21 +456,71 @@ HBS_API zlx_file_status_t ZLX_CALL hbs_file_close
     zlx_file_t * f
 );
 
+/* hbs_log_init *************************************************************/
+/**
+ *  Initializes the global logger of this library.
+ *  The global logger is exposed as #hbs_log.
+ */
 HBS_API void hbs_log_init (zlx_file_t * ZLX_RESTRICT file, unsigned int level);
 
+/* hbs_log ******************************************************************/
+/**
+ *  Global logger maintained by this library.
+ */
 extern HBS_API zlx_log_t * hbs_log;
 
+/*  HBS_LF  */
+/**
+ *  Logs a fault message.
+ */
 #define HBS_LF(...) (ZLX_LF(hbs_log, __VA_ARGS__))
+
+/*  HBS_LE  */
+/**
+ *  Logs an error message.
+ */
 #define HBS_LE(...) (ZLX_LE(hbs_log, __VA_ARGS__))
+
+/*  HBS_LW  */
+/**
+ *  Logs a warning message.
+ */
 #define HBS_LW(...) (ZLX_LW(hbs_log, __VA_ARGS__))
+
+/*  HBS_LI  */
+/**
+ *  Logs an informational message.
+ */
 #define HBS_LI(...) (ZLX_LI(hbs_log, __VA_ARGS__))
+
+
+/*  HBS_LD  */
+/**
+ *  Logs a debug message.
+ *  This macro generates code only on debug builds (_DEBUG defined as non-zero)
+ */
 #define HBS_LD(...) (ZLX_LD(hbs_log, __VA_ARGS__))
 
+/*  HBS_DM  */
+/**
+ *  Decorated debug message.
+ *  This macro generates debug messages prefixed by the code location
+ */
 #define HBS_DM(_fmt, ...) \
     (ZLX_LD(hbs_log, "$s:$i:$s(): " _fmt "\n", \
             __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__))
 
+/*  HBS_DMX  */
+/**
+ *  Provides a quick and convenient way of disabling instances of #HBS_DM.
+ */
 #define HBS_DMX(_fmt, ...) ((void) 0)
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
 
 #endif
 
